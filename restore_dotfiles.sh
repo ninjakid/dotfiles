b02b777071ba9c8af741fa1590ad0dbd17370083
adit <adityawinanda2@gmail.com>
2015-08-24 09:29:23 +0700

    add vim folder

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -3,7 +3,7 @@
 # variables
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="bashrc vimrc zshrc oh-my-zsh aliases_winanda"
+files="bashrc vimrc vim zshrc oh-my-zsh aliases_winanda"
 
 
 # create dotfiles_old in homedir
